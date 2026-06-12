4f1681d417d961f580ee3bf0c17f6967d3d0027f
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 18:03:19 +0200

    rotate face clock and counter clockwise

--- src/scripts/rubiks-cube-rotation.ts
+++ src/scripts/rubiks-cube-rotation.ts
@@ -1,5 +1,23 @@
 import type { RubiksCube } from "./rubiks-cube-model";
 
+// support functions for clock/counter clockwise rotation
+function rotateFaceClockwise(face: string[][]): string[][] {
+    return [
+        [face[2][0], face[1][0], face[0][0]],
+        [face[2][1], face[1][1], face[0][1]],
+        [face[2][2], face[1][2], face[0][2]]
+    ]
+}
+
+function rotateFaceCounterClockwise(face: string[][]): string[][] {
+    return [
+        [face[0][2], face[1][2], face[2][2]],
+        [face[0][1], face[1][1], face[2][1]],
+        [face[0][0], face[1][0], face[2][0]]
+    ]
+}
+
+// rotation algorithms
 /*
 function upFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
     
