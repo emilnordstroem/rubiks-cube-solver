7d31b4bb6f4cee34152a105434ea0596120544cf
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 21:17:48 +0200

    Flow of daisy algorithm under construction

--- src/scripts/rubiks-cube-algorithms/rubiks-cube-daisy-algorithm.ts
+++ src/scripts/rubiks-cube-algorithms/rubiks-cube-daisy-algorithm.ts
@@ -1,11 +1,10 @@
 import type { RubiksCube } from "../rubiks-cube-model";
 
+/*
 export function daisyAlgorithm (rubikscube: RubiksCube): RubiksCube {
-    if(isDaisyComplete(rubikscube.Down)){
-        return rubikscube
-    }
     
 }
+*/
 
 const center: string = 'yellow'
 const leaf: string = 'white' 
