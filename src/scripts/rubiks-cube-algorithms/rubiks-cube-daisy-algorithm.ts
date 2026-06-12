41434a7b58bb50aaa41c5395f8cfd81d612638af
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 20:31:11 +0200

    Refactoring and dailsy algorithm under construction

---
+++ src/scripts/rubiks-cube-algorithms/rubiks-cube-daisy-algorithm.ts
@@ -0,0 +1,7 @@
+import type { RubiksCube } from "../rubiks-cube-model";
+
+export function daisyAlgorithm (rubikscube: RubiksCube): RubiksCube {
+    if(){}
+}
+
+function isDaisyComplete(face: string[][])
--- src/scripts/rubiks-cube-rotations/rubiks-cube-rotation-support.ts
+++ src/scripts/rubiks-cube-rotations/rubiks-cube-rotation-support.ts
@@ -14,12 +14,12 @@ export function rotateFaceCounterClockwise(face: string[][]): string[][] {
     ]
 }
 
-export function getColumn(face: string[][], col: number): string[] {
-    return [face[0][col], face[1][col], face[2][col]];
+export function getColumn(face: string[][], column: number): string[] {
+    return [face[0][column], face[1][column], face[2][column]];
 }
  
-export function setColumn(face: string[][], col: number, values: string[]): void {
-    face[0][col] = values[0];
-    face[1][col] = values[1];
-    face[2][col] = values[2];
+export function setColumn(face: string[][], column: number, colors: string[]): void {
+    face[0][column] = colors[0];
+    face[1][column] = colors[1];
+    face[2][column] = colors[2];
 }
